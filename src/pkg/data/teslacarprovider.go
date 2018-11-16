f9e9509d28f74dc11f37926f78a76e8dd2cd52cd
Steve Butler <sbutler@ea.com>
2018-11-16 10:12:24 -0800

    First round integration with ElectricGopher

--- src/pkg/data/teslacarprovider.go
+++ src/pkg/data/teslacarprovider.go
@@ -1,5 +1,7 @@
 package data
 
+import "github.com/stephbu/electricgopher/api"
+
 type TeslaCarProvider struct {
 	// External parameters
 	VIN         string // API interactions needs to be tied
@@ -7,9 +9,17 @@ type TeslaCarProvider struct {
 
 	// Internal state
 	initialized   bool
+	vehicleId     int64
 	hasDriveState bool
 	apiEndpoint   string
+
+	client *api.Client
 }
+
+const (
+	TESLA_CLIENT_ID     = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
+	TESLA_CLIENT_SECRET = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
+)
 
 func NewTeslaCarProvider(vin string, username string, password string) *TeslaCarProvider {
 	teslaCarProvider := &TeslaCarProvider{VIN: vin, Credentials: UsernamePasswordCredential{Username: username, Password: password}}
@@ -38,7 +48,18 @@ func (tesla *TeslaCarProvider) initialize() error {
 		return nil
 	}
 
+	tesla.client = api.NewClient(TESLA_CLIENT_ID, TESLA_CLIENT_SECRET, tesla.Credentials.Username, tesla.Credentials.Password, "https://owner-api.teslamotors.com/", nil)
+	vehicles, err := tesla.client.GetVehicles()
+	if err != nil {
+		return err
+	}
+
 	// Get Vehicle
+	for _, vehicle := range vehicles.Response {
+		if vehicle.Vin == tesla.VIN {
+			tesla.vehicleId = vehicle.Id
+		}
+	}
 
 	tesla.initialized = true
 	return nil
--- src/pkg/data/teslacarprovider_test.go
+++ src/pkg/data/teslacarprovider_test.go
@@ -1,1 +1,22 @@
 package data
+
+import (
+	"os"
+	"testing"
+)
+
+const (
+	TESLA_VIN      = "TESLA_VIN"
+	TESLA_USERNAME = "TESLA_USERNAME"
+	TESLA_PASSWORD = "TESLA_PASSWORD"
+)
+
+func TestNewTeslaCarProvider(t *testing.T) {
+
+	teslaVin := os.Getenv(TESLA_VIN)
+	teslaUsername := os.Getenv(TESLA_USERNAME)
+	teslaPassword := os.Getenv(TESLA_PASSWORD)
+
+	teslaCarProvider := NewTeslaCarProvider(teslaVin, teslaUsername, teslaPassword)
+	teslaCarProvider.initialize()
+}
