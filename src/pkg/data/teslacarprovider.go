4b00f917441cb457cb7c6d869d54c2e91959311d
Steve Butler <stephbu@gmail.com>
2018-11-19 11:33:44 -0800

    TeslaCarProvider tests, and fixes. Car WakeUp retry loops and testing

--- src/pkg/data/teslacarprovider.go
+++ src/pkg/data/teslacarprovider.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/stephbu/electricgopher/api"
 	"strconv"
+	"time"
 )
 
 type TeslaCarProvider struct {
@@ -27,10 +28,11 @@ type TeslaCarProvider struct {
 
 const (
 	// TODO: move somewhat static tesla secrets into environment variables, just in case.
-	TESLA_CLIENT_ID      = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
-	TESLA_CLIENT_SECRET  = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
-	VEHICLE_STATE_ASLEEP = "asleep"
-	VEHICLE_STATE_ONLINE = "online"
+	TESLA_CLIENT_ID       = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
+	TESLA_CLIENT_SECRET   = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
+	VEHICLE_STATE_ASLEEP  = "asleep"
+	VEHICLE_STATE_ONLINE  = "online"
+	VEHICLE_STATE_OFFLINE = "offline"
 )
 
 func NewTeslaCarProvider(config *Configuration) *TeslaCarProvider {
@@ -62,7 +64,7 @@ func (tesla *TeslaCarProvider) initialize() error {
 
 	logger := logrus.StandardLogger()
 
-	tesla.client = api.NewClient(TESLA_CLIENT_ID, TESLA_CLIENT_SECRET, tesla.Credentials.Username, tesla.Credentials.Password, "https://owner-api.teslamotors.com/", logger)
+	tesla.client = api.NewClient(TESLA_CLIENT_ID, TESLA_CLIENT_SECRET, tesla.Credentials.Username, tesla.Credentials.Password, "https://owner-api.teslamotors.com", logger)
 	vehicles, err := tesla.client.GetVehicles()
 	if err != nil {
 		logger.Error(err)
@@ -75,38 +77,61 @@ func (tesla *TeslaCarProvider) initialize() error {
 		if vehicle.Vin == tesla.VIN {
 			tesla.vehicleId = strconv.FormatInt(vehicle.Id, 10)
 			tesla.vehicleState = vehicle.State
-			logger.Printf("VIN:%v Vehicle ID: %v", tesla.VIN, tesla.vehicleId)
+			logger.Printf("VIN:%v Vehicle ID: %+v", tesla.VIN, tesla.vehicleId)
 			matchingCar = true
 		}
 	}
 
 	if !matchingCar {
-		return errors.New(fmt.Sprintf("no car matching VIN:%v", tesla.VIN))
+		return errors.New(fmt.Sprintf("no car matching VIN:%+v", tesla.VIN))
 	}
 
-	if tesla.vehicleState == VEHICLE_STATE_ASLEEP {
-		response, err := tesla.client.WakeUp(tesla.vehicleId)
-		if err != nil {
-			logger.Printf("WakeUp %v", err)
-			return err
+	awake := false
+	retryCount := 15
+	retryThrottle := time.Tick(time.Second)
+
+	for !awake && retryCount > 0 {
+
+		logger.Printf("Current Car State: %+v (remaining attempts: %d)", tesla.vehicleState, retryCount)
+
+		switch tesla.vehicleState {
+		case VEHICLE_STATE_OFFLINE, VEHICLE_STATE_ASLEEP:
+			logger.Print("Waking car")
+			wakeUpResponse, err := tesla.client.WakeUp(tesla.vehicleId)
+			if err != nil {
+				logger.Printf("WakeUp %+v", err)
+				return err
+			}
+			tesla.vehicleState = wakeUpResponse.Response.State
+			logger.Printf("Wake Up Response %+v", wakeUpResponse)
+		case VEHICLE_STATE_ONLINE:
+			awake = true
 		}
-		logger.Printf("%v", response)
+
+		if !awake {
+			retryCount--
+			<-retryThrottle
+		}
+	}
+
+	if !awake {
+		return errors.New("Could not wake car in allotted time")
 	}
 
 	drive, err := tesla.client.GetDriveState(tesla.vehicleId)
 	if err != nil {
-		logger.Printf("GetDriveState %v", err)
+		logger.Printf("GetDriveState %+v", err)
 		return err
 	}
-	logger.Printf("%v", drive.Response)
+	logger.Printf("%+v", drive.Response)
 	tesla.vehicleLocation = LatLong{Lat: drive.Response.Latitude, Long: drive.Response.Longitude}
 
 	vehicle, err := tesla.client.GetVehicleState(tesla.vehicleId)
 	if err != nil {
-		logger.Printf("GetVehicleState %v", err)
+		logger.Printf("GetVehicleState %+v", err)
 		return err
 	}
-	logger.Printf("%v", vehicle.Response)
+	logger.Printf("%+v", vehicle.Response)
 
 	tesla.initialized = true
 	return nil
--- src/pkg/data/teslacarprovider_test.go
+++ src/pkg/data/teslacarprovider_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestNewTeslaCarProviderInitialize(t *testing.T) {
-
 	config, err := LoadConfigFromEnv()
 	assert.Nil(t, err)
 
@@ -38,6 +37,9 @@ func TestLoadConfigFromEnv1(t *testing.T) {
 	assert.Equal(t, config.VIN, VIN)
 	assert.Equal(t, config.Username, USERNAME)
 	assert.Equal(t, config.Password, PASSWORD)
+	assert.Equal(t, config.FenceLatLong.Lat, float64(47.642744))
+	assert.Equal(t, config.FenceLatLong.Long, float64(-122.112782))
+	assert.Equal(t, config.FenceRadius, float64(30))
 }
 
 func TestLoadConfigFromEnv2(t *testing.T) {
@@ -51,8 +53,8 @@ func TestLoadConfigFromEnv2(t *testing.T) {
 	os.Setenv(TESLA_VIN, VIN)
 	os.Setenv(TESLA_USERNAME, USERNAME)
 	os.Setenv(TESLA_PASSWORD, PASSWORD)
-	os.Setenv(FENCE_RADIUS, RADIUS)
 	os.Setenv(FENCE_LATLONG, LATLONG)
+	os.Setenv(FENCE_RADIUS, RADIUS)
 
 	config, err := LoadConfigFromEnv()
 	assert.Error(t, err)
